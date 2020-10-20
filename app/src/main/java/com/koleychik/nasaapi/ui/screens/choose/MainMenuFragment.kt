c4f9f2c7fbde35e137c75eb0d8a3f2d3019dbd6a
KoleychikIvan <koleychikmrevan@mail.ru>
2020-10-20 17:50:54 +0200

    Update 3

--- app/src/main/java/com/koleychik/nasaapi/ui/screens/choose/MainMenuFragment.kt
+++ app/src/main/java/com/koleychik/nasaapi/ui/screens/choose/MainMenuFragment.kt
@@ -1,16 +1,13 @@
 package com.koleychik.nasaapi.ui.screens.choose
 
 import android.os.Bundle
-import android.util.DisplayMetrics
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.koleychik.nasaapi.R
-import com.koleychik.nasaapi.utils.VideoUtils
 import kotlinx.android.synthetic.main.fragment_main_menu.view.*
-
 
 class MainMenuFragment : Fragment() {
 
@@ -32,12 +29,6 @@ class MainMenuFragment : Fragment() {
                 R.id.marsImages -> Navigation.findNavController(requireView())
                     .navigate(R.id.action_mainMenuFragment_to_marsFragment)
 
-//                R.id.nasaImages -> Navigation.findNavController(requireView())
-//                    .navigate(R.id.action_mainMenuFragment_to_nasaImagesFragment)
-//
-//                R.id.nasaVideo -> Navigation.findNavController(requireView())
-//                    .navigate(R.id.action_mainMenuFragment_to_nasaVideosFragment)
-
                 R.id.earthImages -> Navigation.findNavController(requireView())
                     .navigate(R.id.action_mainMenuFragment_to_earthImagesFragment)
             }
@@ -46,33 +37,5 @@ class MainMenuFragment : Fragment() {
         requireView().marsImages.setOnClickListener(onClickListener)
         requireView().earthImages.setOnClickListener(onClickListener)
 
-    }
-
-    private fun workWithVideoView() {
-        val metrics = DisplayMetrics()
-        requireActivity().windowManager.defaultDisplay.getMetrics(metrics)
-        val display = requireActivity().windowManager.defaultDisplay
-//        stageWidth = display.width
-//        stageHeight = display.getHeigth()
-        val params = requireView().video.layoutParams
-        params.width = display.width
-        params.height = display.height
-
-        requireView().video.apply {
-            layoutParams = params
-            setVideoURI(VideoUtils.getUri(requireContext().packageName, R.raw.main_manu_bg_video))
-            start()
-        }
-    }
-
-    override fun onStart() {
-        super.onStart()
-        workWithVideoView()
-        requireView().video.resume()
-    }
-
-    override fun onStop() {
-        super.onStop()
-        requireView().video.stopPlayback()
     }
 }
